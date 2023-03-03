a3477d3be98c306c770072fbe6cb3e056fe83638
symplifygh <symplifyinstructors@gmail.com>
2023-03-03 12:49:58 +0530

    Added date time

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 
 URL="https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=INR"
 HTML_TEMPLATE = '''
@@ -11,20 +12,19 @@ HTML_TEMPLATE = '''
 </head>
 <body>
     <h1>1 USD = {}</h1>
+    <p>Updated at {}</p>
 </body>
 </html>
 '''
 
-
 response = requests.get(URL)
-
-
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
     tag = soup.find_all("p", class_="result__BigRate-sc-1bsijpp-1 iGrAod")[0]
     value = tag.text
+    currentdate = datetime.datetime.now()
     with open("index.html", "w") as f:
-        f.write(HTML_TEMPLATE.format(value))
+        f.write(HTML_TEMPLATE.format(value, currentdate))
 else:
     print("Request Failed")
